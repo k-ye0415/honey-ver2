03f7360505f6eaf93d9cfe5b73290b9bee12b428
YEJIN <ye_s_jin@naver.com>
2025-05-30 16:10:45 +0900

    firstLaunch시에 firestore data 저장 로직 추가

--- app/src/main/java/com/jin/honey/feature/food/domain/FoodRepository.kt
+++ app/src/main/java/com/jin/honey/feature/food/domain/FoodRepository.kt
@@ -5,6 +5,7 @@ import com.jin.honey.feature.food.domain.model.CategoryType
 import com.jin.honey.feature.food.domain.model.Menu
 
 interface FoodRepository {
+    suspend fun syncAllMenu()
     suspend fun findCategories(): Result<List<CategoryType>>
     suspend fun findAllCategoryMenus(): Result<List<Category>>
     suspend fun findMenuIngredient(menuName: String): Result<Menu>
---
+++ app/src/main/java/com/jin/honey/feature/onboarding/ui/OnboardingViewModel.kt
@@ -0,0 +1,33 @@
+package com.jin.honey.feature.onboarding.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jin.honey.feature.datastore.PreferencesRepository
+import com.jin.honey.feature.food.domain.usecase.SyncAllMenuUseCase
+import kotlinx.coroutines.launch
+
+class OnboardingViewModel(
+    private val syncAllMenuUseCase: SyncAllMenuUseCase,
+    private val repository: PreferencesRepository
+) : ViewModel() {
+    private var isFirstLaunch = true
+
+    init {
+        checkIfFirstLaunch()
+    }
+
+    fun onLocationPermissionGranted() {
+        viewModelScope.launch {
+            if (isFirstLaunch) {
+                repository.completeFirstLaunch()
+                syncAllMenuUseCase()
+            }
+        }
+    }
+
+    private fun checkIfFirstLaunch() {
+        viewModelScope.launch {
+            isFirstLaunch = repository.isFirstLaunch()
+        }
+    }
+}
