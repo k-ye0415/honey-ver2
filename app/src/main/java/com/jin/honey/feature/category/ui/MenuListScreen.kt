7d01609811c8bab12765231fcecaa5f468919b70
YEJIN <ye_s_jin@naver.com>
2025-05-31 17:23:03 +0900

    menuList screen ui

--- app/src/main/java/com/jin/honey/feature/category/ui/MenuListScreen.kt
+++ app/src/main/java/com/jin/honey/feature/category/ui/MenuListScreen.kt
@@ -1,26 +1,36 @@
 package com.jin.honey.feature.category.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.jin.honey.feature.food.domain.model.Food
 import com.jin.honey.feature.food.domain.model.CategoryType
+import com.jin.honey.feature.food.domain.model.Food
 import com.jin.honey.feature.food.domain.model.Ingredient
 import com.jin.honey.feature.food.domain.model.Menu
 import com.jin.honey.ui.theme.HoneyTheme
@@ -29,7 +39,7 @@ import com.jin.honey.ui.theme.HoneyTheme
 fun MenuListScreen(menuList: List<Menu>, onNavigateToIngredient: (menuName: String) -> Unit) {
     LazyColumn(
         modifier = Modifier.fillMaxWidth(),
-        contentPadding = PaddingValues(vertical = 4.dp),
+        contentPadding = PaddingValues(vertical = 14.dp),
     ) {
         items(menuList.size) { index ->
             val menu = menuList[index]
@@ -37,7 +47,8 @@ fun MenuListScreen(menuList: List<Menu>, onNavigateToIngredient: (menuName: Stri
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(bottom = 4.dp)
+                    .padding(horizontal = 20.dp)
+                    .padding(bottom = 10.dp)
                     .clickable { onNavigateToIngredient(menu.name) }
             ) {
                 AsyncImage(
@@ -46,13 +57,64 @@ fun MenuListScreen(menuList: List<Menu>, onNavigateToIngredient: (menuName: Stri
                     modifier = Modifier
                         .padding(end = 10.dp)
                         .size(100.dp)
+                        .clip(RoundedCornerShape(8.dp))
                         .background(Color.LightGray),
                     contentScale = ContentScale.Crop
                 )
-                Column {
-                    Text(menu.name)
-                    Button({}) {
-                        Text("모든 재료 담기")
+                Column(modifier = Modifier.fillMaxHeight()) {
+                    Text(
+                        text = menu.name,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(bottom = 4.dp)
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        val interactionSource = remember { MutableInteractionSource() }
+                        Box(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(30.dp))
+                                .indication(
+                                    interactionSource = interactionSource,
+                                    indication = rememberRipple(
+                                        color = Color.Red,
+                                        bounded = true,
+                                    )
+                                )
+                                .clickable(
+                                    interactionSource = interactionSource,
+                                    indication = null,
+                                    onClick = { /* 클릭 처리 */ }
+                                )
+                                .border(1.dp, Color.Red, RoundedCornerShape(30.dp))
+                                .padding(start = 8.dp, end = 8.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("레시피 보기", fontSize = 12.sp)
+                        }
+                        Box(
+                            modifier = Modifier
+                                .clip(RoundedCornerShape(30.dp))
+                                .indication(
+                                    interactionSource = interactionSource,
+                                    indication = rememberRipple(
+                                        color = Color.Red,
+                                        bounded = true,
+                                    )
+                                )
+                                .clickable(
+                                    interactionSource = interactionSource,
+                                    indication = null,
+                                    onClick = { /* 클릭 처리 */ }
+                                )
+                                .border(1.dp, Color.Red, RoundedCornerShape(30.dp))
+                                .padding(start = 8.dp, end = 8.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("모든 재료 담기", fontSize = 12.sp)
+                        }
                     }
                 }
             }
