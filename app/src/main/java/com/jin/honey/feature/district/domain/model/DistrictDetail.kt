46c23490bf3aca0e6b220ab9c17c0914f010704e
YEJIN <ye_s_jin@naver.com>
2025-06-06 22:03:15 +0900

    district 관련 모델 추가

---
+++ app/src/main/java/com/jin/honey/feature/district/domain/model/DistrictDetail.kt
@@ -0,0 +1,5 @@
+package com.jin.honey.feature.district.domain.model
+
+data class DistrictDetail(
+    val detailAddress: String
+)
---
+++ app/src/main/java/com/jin/honey/feature/district/domain/model/UserDistrict.kt
@@ -0,0 +1,13 @@
+package com.jin.honey.feature.district.domain.model
+
+data class UserDistrict(
+    val districtType: DistrictType,
+    val district: District,
+    val districtDetail: DistrictDetail
+)
+
+enum class DistrictType(val typeName: String) {
+    HOME("HOME"),
+    CURRENT("CURRENT"),
+    HISTORY("HISTORY")
+}
