2b3b47eccdb95864c0e7a4c6a6daa832f51e9558
YEJIN <ye_s_jin@naver.com>
2025-06-11 01:51:46 +0900

    ui 정리

--- app/src/main/java/com/jin/honey/feature/paymentdetail/ui/PaymentDetailScreen.kt
+++ app/src/main/java/com/jin/honey/feature/paymentdetail/ui/PaymentDetailScreen.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForwardIos
@@ -28,6 +32,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,7 +48,6 @@ import com.jin.honey.feature.payment.domain.model.PayPrice
 import com.jin.honey.feature.payment.domain.model.Payment
 import com.jin.honey.feature.payment.domain.model.PaymentState
 import com.jin.honey.feature.payment.domain.model.Requirement
-import com.jin.honey.feature.review.ui.fallbackData
 import com.jin.honey.ui.theme.HoneyTheme
 import com.jin.honey.ui.theme.PointColor
 import java.time.Instant
@@ -59,82 +63,185 @@ fun PaymentDetailScreen(paymentId: Int) {
                     tint = Color.Black
                 )
             }
-            Row {
-                Text(text = paymentFallback.cart.firstOrNull()?.menuName.orEmpty())
-                Icon(Icons.Default.ArrowForwardIos, contentDescription = "")
-            }
-            Row {
-                CustomBoxButton(
-                    modifier = Modifier.weight(1f),
-                    rippleColor = PointColor,
-                    borderColor = PointColor,
-                    btnText = "재주문",
-                    textColor = PointColor,
-                    fontWeight = FontWeight.Normal,
-                    onClickButton = {}
-                )
-                CustomBoxButton(
-                    modifier = Modifier.weight(1f),
-                    rippleColor = PointColor,
-                    borderColor = PointColor,
-                    btnText = "리뷰쓰기",
-                    textColor = PointColor,
-                    fontWeight = FontWeight.Normal,
-                    onClickButton = {}
-                )
-            }
-            HorizontalDivider()
-            Text("주문자 정보")
-            Row {
-                Text("주소")
-                Column {
-                    Text("${paymentFallback.address.address.addressName.lotNumAddress} ${paymentFallback.address.addressDetail}")
-                    Text("[도로명] ${paymentFallback.address.address.addressName.roadAddress} ${paymentFallback.address.addressDetail}")
+            LazyColumn {
+                item {
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(top = 16.dp, bottom = 14.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = paymentFallback.cart.firstOrNull()?.menuName.orEmpty(),
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Icon(Icons.Default.ArrowForwardIos, contentDescription = "", modifier = Modifier.size(18.dp))
+                    }
+                    Row(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(bottom = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        CustomBoxButton(
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(end = 4.dp),
+                            rippleColor = PointColor,
+                            borderColor = PointColor,
+                            btnText = "재주문",
+                            textColor = PointColor,
+                            fontWeight = FontWeight.Normal,
+                            onClickButton = {}
+                        )
+                        CustomBoxButton(
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(start = 4.dp),
+                            rippleColor = PointColor,
+                            borderColor = PointColor,
+                            btnText = "리뷰쓰기",
+                            textColor = PointColor,
+                            fontWeight = FontWeight.Normal,
+                            onClickButton = {}
+                        )
+                    }
+                    HorizontalDivider()
                 }
-            }
-            Row {
-                Text("가게 요청사항")
-                Text(paymentFallback.requirement.requirement)
-            }
-            Row {
-                Text("라이더 요청사항")
-                Text(paymentFallback.requirement.riderRequirement)
-            }
-            HorizontalDivider()
-            Text("주문내역")
-            for (menus in paymentFallback.cart) {
-                Text(menus.menuName)
-                for (ingredient in menus.ingredients) {
-                    Row {
-                        Text(ingredient.name)
-                        Text("x ${ingredient.cartQuantity}")
-                        Text("${(ingredient.cartQuantity * ingredient.unitPrice)}")
+                item {
+                    Text(
+                        text = "주문자 정보",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(top = 14.dp, bottom = 8.dp)
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp, vertical = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("주소")
+                        Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
+                            Text(text = "${paymentFallback.address.address.addressName.lotNumAddress} ${paymentFallback.address.addressDetail}")
+                            Text(
+                                text = "[도로명] ${paymentFallback.address.address.addressName.roadAddress} ${paymentFallback.address.addressDetail}",
+                                fontSize = 14.sp,
+                                textAlign = TextAlign.End
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp, vertical = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("가게 요청사항")
+                        Text(paymentFallback.requirement.requirement, modifier = Modifier.weight(1f), textAlign = TextAlign.End)
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 10.dp, vertical = 4.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("라이더 요청사항")
+                        Text(
+                            paymentFallback.requirement.riderRequirement,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.End
+                        )
+                    }
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 14.dp))
+                }
+                item {
+                    Text(
+                        "주문내역",
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(bottom = 8.dp)
+                    )
+                    Column(
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(bottom = 14.dp)
+                    ) {
+                        for (menus in paymentFallback.cart) {
+                            Text(menus.menuName)
+                            for (ingredient in menus.ingredients) {
+                                Row {
+                                    Text("${ingredient.name} ${ingredient.quantity}")
+                                    Text("x ${ingredient.cartQuantity}")
+                                    Spacer(Modifier.weight(1f))
+                                    Text("${(ingredient.cartQuantity * ingredient.unitPrice)}")
+                                }
+                            }
+                        }
+                    }
+
+                }
+                item {
+                    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        Text("상품금액", fontWeight = FontWeight.SemiBold)
+                        Spacer(Modifier.weight(1f))
+                        Text(
+                            text = stringResource(
+                                R.string.order_detail_product_price_monetary,
+                                paymentFallback.prices.productPrice
+                            ),
+                            fontWeight = FontWeight.SemiBold,
+                        )
+                    }
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 14.dp))
+                    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        Text("배달요금")
+                        Spacer(Modifier.weight(1f))
+                        Text(
+                            text = stringResource(
+                                R.string.order_detail_product_price_monetary,
+                                paymentFallback.prices.deliveryPrice
+                            )
+                        )
+                    }
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 14.dp))
+                    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        Text("총 결제금액", fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+                        Spacer(Modifier.weight(1f))
+                        Text(
+                            text = stringResource(
+                                R.string.order_detail_product_price_monetary,
+                                paymentFallback.prices.totalPrice
+                            ),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.SemiBold,
+                        )
+                    }
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 14.dp))
+                }
+                item {
+                    Text(
+                        "주문정보", fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                            .padding(bottom = 8.dp)
+                    )
+                    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        Text("주문번호", fontSize = 12.sp)
+                        Spacer(Modifier.weight(1f))
+                        Text("123904r9032", fontSize = 12.sp)
+                    }
+                    Row(modifier = Modifier.padding(horizontal = 10.dp)) {
+                        Text("주문시간", fontSize = 12.sp)
+                        Text(paymentFallback.payInstant.toEpochMilli().toString(), fontSize = 12.sp)
                     }
                 }
-            }
-            Row {
-                Text("상품금액")
-                Text("${paymentFallback.prices.productPrice}")
-            }
-            HorizontalDivider()
-            Row {
-                Text("배달요금")
-                Text("${paymentFallback.prices.deliveryPrice}")
-            }
-            HorizontalDivider()
-            Row {
-                Text("총 결제금액")
-                Text("${paymentFallback.prices.totalPrice}")
-            }
-            HorizontalDivider()
-            Text("주문정보")
-            Row {
-                Text("주문번호")
-                Text("123904r9032")
-            }
-            Row {
-                Text("주문시간")
-                Text(paymentFallback.payInstant.toEpochMilli().toString())
             }
         }
     }
@@ -153,7 +260,7 @@ fun CustomBoxButton(
     val interactionSource = remember { MutableInteractionSource() }
     Box(
         modifier = modifier
-            .clip(RoundedCornerShape(4.dp))
+            .clip(RoundedCornerShape(8.dp))
             .background(Color.White)
             .indication(
                 interactionSource,
@@ -164,7 +271,7 @@ fun CustomBoxButton(
                 indication = null,
                 onClick = onClickButton
             )
-            .border(1.dp, borderColor, RoundedCornerShape(4.dp))
+            .border(1.dp, borderColor, RoundedCornerShape(8.dp))
             .padding(vertical = 8.dp),
         contentAlignment = Alignment.Center
     ) {
