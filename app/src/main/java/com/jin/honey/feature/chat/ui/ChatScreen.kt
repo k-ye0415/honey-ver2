93116f0c6b60e3c9478fe7218c4134f7089404e4
YEJIN <ye_s_jin@naver.com>
2025-06-30 17:45:23 +0900

    최근 메세지 돌아가기 기능 추가

--- app/src/main/java/com/jin/honey/feature/chat/ui/ChatScreen.kt
+++ app/src/main/java/com/jin/honey/feature/chat/ui/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.jin.honey.feature.chat.ui
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -15,13 +16,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -29,9 +31,11 @@ import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,7 +50,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
-import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.jin.honey.R
 import com.jin.honey.feature.openai.domain.ChatItem
@@ -55,7 +58,9 @@ import com.jin.honey.ui.theme.DistrictSearchBoxBackgroundColor
 import com.jin.honey.ui.theme.DistrictSearchHintTextColor
 import com.jin.honey.ui.theme.IncomingBubbleBackgroundColor
 import com.jin.honey.ui.theme.OutgoingBubbleBackgroundColor
+import com.jin.honey.ui.theme.PayDetailBoxBorderColor
 import com.jin.honey.ui.theme.PointColor
+import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -64,6 +69,18 @@ import java.util.Locale
 @Composable
 fun ChatScreen(viewModel: ChatViewModel, menuName: String) {
     val messageItems = viewModel.messagePagingFlow(menuName).collectAsLazyPagingItems()
+
+    val listState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    val showScrollToBottomButton by remember {
+        derivedStateOf {
+            val firstVisibleIndex = listState.firstVisibleItemIndex
+            val isAtBottom = firstVisibleIndex == 0
+
+            messageItems.itemCount > 0 && !isAtBottom
+        }
+    }
 
     Scaffold(
         modifier = Modifier
@@ -76,33 +93,47 @@ fun ChatScreen(viewModel: ChatViewModel, menuName: String) {
                 .padding(innerPadding)
         ) {
             ChatBotHeader(menuName)
-            LazyColumn(
+            Box(
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxWidth(),
-                reverseLayout = true, // 최신 메시지를 아래쪽에 표시
-                verticalArrangement = Arrangement.Bottom,
-                contentPadding = PaddingValues(vertical = 10.dp),
+                    .fillMaxWidth()
             ) {
-                if (messageItems.loadState.append is LoadState.Loading || messageItems.loadState.refresh is LoadState.Loading) {
-                    item {
-                        CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+                LazyColumn(
+                    state = listState,
+                    reverseLayout = true,
+                    verticalArrangement = Arrangement.Bottom,
+                    contentPadding = PaddingValues(vertical = 10.dp),
+                ) {
+                    items(messageItems.itemCount) { index ->
+                        val item = messageItems[index]
+                        if (item != null) {
+                            ChatBotList(item)
+                        } else {
+                            // FIXME UI
+                        }
                     }
                 }
 
-                items(messageItems.itemCount) { index ->
-                    messageItems[index]?.let {
-                        ChatBotList(it)
+                this@Column.AnimatedVisibility(
+                    visible = showScrollToBottomButton,
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(16.dp)
+                ) {
+                    IconButton(
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = PayDetailBoxBorderColor,
+                            contentColor = PointColor,
+                        ),
+                        onClick = {
+                            coroutineScope.launch {
+                                listState.animateScrollToItem(0)
+                            }
+                        },
+                    ) {
+                        Icon(Icons.Default.KeyboardArrowDown, contentDescription = "최신 메시지 버튼")
                     }
                 }
-
-//                messageItems.apply {
-//                    when {
-//                        loadState.refresh is LoadState.Loading -> { /* 첫 로딩 */ }
-//                        loadState.append is LoadState.Loading -> { /* 다음 페이지 로딩 */ }
-//                        loadState.append is LoadState.Error -> { /* 에러 처리 */ }
-//                    }
-//                }
             }
             ChatBotInput(onSendMessage = { viewModel.sendMessage(menuName, it) })
         }
