07fcbe07178ed6ce74def4ffa82336ddd0203b71
YEJIN <ye_s_jin@naver.com>
2025-07-02 15:05:44 +0900

    주문 상세 화면에 CustomBoxButton 적용

--- ui/src/main/java/com/jin/ui/payment/content/PayDetailOverView.kt
+++ ui/src/main/java/com/jin/ui/payment/content/PayDetailOverView.kt
@@ -1,27 +1,20 @@
 package com.jin.ui.payment.content
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.jin.BoxButton
 import com.jin.ui.R
 import com.jin.ui.theme.PayDetailBoxBorderColor
 import com.jin.ui.theme.PayDetailDividerColor
@@ -44,59 +37,33 @@ fun PayDetailOverView(menuName: String) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         // FIXME reorder
-        CustomBoxButton(
+        BoxButton(
             modifier = Modifier
                 .weight(1f)
                 .padding(end = 4.dp),
-            rippleColor = PointColor,
+            shape = RoundedCornerShape(8.dp),
+            backgroundColor = Color.White,
             borderColor = PointColor,
-            btnText = stringResource(R.string.order_history_reorder),
-            textColor = PointColor,
-            onClickButton = {}
-        )
+            rippleColor = PointColor,
+            contentPadding = PaddingValues(vertical = 8.dp),
+            onClick = {},
+        ) {
+            Text(text = stringResource(R.string.order_history_reorder), fontSize = 14.sp, color = PointColor)
+        }
         // FIXME review write
-        CustomBoxButton(
+        BoxButton(
             modifier = Modifier
                 .weight(1f)
                 .padding(start = 4.dp),
-            rippleColor = Color.Gray,
+            shape = RoundedCornerShape(8.dp),
+            backgroundColor = Color.White,
             borderColor = PayDetailBoxBorderColor,
-            btnText = stringResource(R.string.order_history_review),
-            textColor = Color.Black,
-            onClickButton = {}
-        )
+            rippleColor = Color.Gray,
+            contentPadding = PaddingValues(vertical = 8.dp),
+            onClick = {},
+        ) {
+            Text(text = stringResource(R.string.order_history_review), fontSize = 14.sp, color =  Color.Black,)
+        }
     }
     HorizontalDivider(color = PayDetailDividerColor)
-}
-
-
-@Composable
-private fun CustomBoxButton(
-    modifier: Modifier,
-    rippleColor: Color,
-    borderColor: Color,
-    btnText: String,
-    textColor: Color,
-    onClickButton: () -> Unit
-) {
-    val interactionSource = remember { MutableInteractionSource() }
-    Box(
-        modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
-            .background(Color.White)
-            .indication(
-                interactionSource,
-                rememberRipple(color = rippleColor, bounded = true)
-            )
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null,
-                onClick = onClickButton
-            )
-            .border(1.dp, borderColor, RoundedCornerShape(8.dp))
-            .padding(vertical = 8.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(btnText, fontSize = 14.sp, color = textColor)
-    }
 }
