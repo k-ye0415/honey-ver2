d4a5d25b8e2a86fbcaa0620d5cd444bd69e95858
YEJIN <ye_s_jin@naver.com>
2025-07-02 15:40:38 +0900

    장바구니의 옵션수정, 주문하기 버튼 적용

--- ui/src/main/java/com/jin/ui/cart/content/CartContent.kt
+++ ui/src/main/java/com/jin/ui/cart/content/CartContent.kt
@@ -2,11 +2,9 @@ package com.jin.ui.cart.content
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,10 +12,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,8 +25,9 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-import com.jin.ui.R
+import com.jin.RoundedBoxButton
 import com.jin.domain.cart.model.Cart
+import com.jin.ui.R
 import com.jin.ui.theme.PointColor
 
 @Composable
@@ -66,64 +63,49 @@ fun CartContent(
                 Text(menuNames, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
                 Text(ingredientNames, fontSize = 14.sp, maxLines = 2, overflow = TextOverflow.Ellipsis)
                 Spacer(Modifier.weight(1f))
-                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.Bottom) {
-                    SubButtonBox(
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    RoundedBoxButton(
                         modifier = Modifier
                             .weight(1f)
                             .padding(end = 2.dp),
-                        btnText = stringResource(R.string.cart_modify_option),
+                        shape = RoundedCornerShape(30.dp),
                         backgroundColor = Color.White,
+                        borderColor = PointColor,
                         rippleColor = PointColor,
-                        textColor = Color.Black,
-                        onClickButton = { onBottomSheetClose(true) }
-                    )
-                    SubButtonBox(
+                        contentPadding = PaddingValues(start = 8.dp, end = 8.dp),
+                        onClick = { onBottomSheetClose(true) }
+                    ) {
+                        Text(
+                            stringResource(R.string.cart_modify_option),
+                            fontSize = 12.sp,
+                            color = Color.Black,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
+                    RoundedBoxButton(
                         modifier = Modifier
                             .weight(1f)
                             .padding(start = 2.dp),
-                        btnText = stringResource(R.string.cart_order),
+                        shape = RoundedCornerShape(30.dp),
                         backgroundColor = PointColor,
+                        borderColor = PointColor,
                         rippleColor = PointColor,
-                        textColor = Color.White,
-                        onClickButton = onNavigateToOrder
-                    )
+                        contentPadding = PaddingValues(start = 8.dp, end = 8.dp),
+                        onClick = onNavigateToOrder
+                    ) {
+                        Text(
+                            stringResource(R.string.cart_order),
+                            fontSize = 12.sp,
+                            color = Color.White,
+                            fontWeight = FontWeight.SemiBold
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-private fun SubButtonBox(
-    modifier: Modifier,
-    btnText: String,
-    backgroundColor: Color,
-    rippleColor: Color,
-    textColor: Color,
-    onClickButton: () -> Unit
-) {
-    val interactionSource = remember { MutableInteractionSource() }
-    Box(
-        modifier = modifier
-            .clip(RoundedCornerShape(30.dp))
-            .background(backgroundColor)
-            .indication(
-                interactionSource = interactionSource,
-                indication = rememberRipple(
-                    color = rippleColor,
-                    bounded = true,
-                )
-            )
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null,
-                onClick = onClickButton
-            )
-            .border(1.dp, PointColor, RoundedCornerShape(30.dp))
-            .padding(start = 8.dp, end = 8.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(btnText, fontSize = 12.sp, color = textColor, fontWeight = FontWeight.SemiBold)
     }
 }
 
